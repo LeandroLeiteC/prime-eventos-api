2509f2cfce3a3ea44d949a3c6e6e9e113a64afa6
LeandroLeiteC <leandrolcosta213@gmail.com>
2020-05-19 13:59:02 -0300

    Login de admin configurado

--- src/main/java/com/leandro/primeeventosapi/PrimeEventosApiApplication.java
+++ src/main/java/com/leandro/primeeventosapi/PrimeEventosApiApplication.java
@@ -5,6 +5,7 @@ import com.leandro.primeeventosapi.domain.entity.Usuario;
 import com.leandro.primeeventosapi.domain.repository.ClienteRepository;
 import com.leandro.primeeventosapi.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,28 +20,38 @@ public class PrimeEventosApiApplication {
     @Autowired
     private PasswordEncoder encoder;
 
+    @Value("${admin.nome}")
+    String nome;
+
+    @Value("${admin.email}")
+    String email;
+
+    @Value("${admin.password}")
+    String password;
+
     @Bean
     public CommandLineRunner run(){
         return args -> {
+
             Usuario usuario = Usuario.builder()
-                    .email("leandro@email.com")
-                    .password(encoder.encode("123456"))
+                    .email(email.toLowerCase())
+                    .password(encoder.encode(password))
                     .admin(true)
                     .ativo(true)
                     .build();
 
             Cliente cliente = Cliente.builder()
                     .usuario(usuario)
-                    .nome("Leandro Leite Costa")
+                    .nome(nome.toUpperCase())
                     .build();
             repository.save(cliente);
 
             System.out.println("");
-            System.out.println("   ====== PRIME EVENTOS API ======");
-            System.out.println("   = LOGIN DE ADMIN              =");
-            System.out.println("   = email: leandro@email.com    =");
-            System.out.println("   = senha: 123456               =");
-            System.out.println("   ===============================");
+            System.out.println("   ========= PRIME EVENTOS API =========");
+            System.out.println("   |          LOGIN DE ADMIN");
+            System.out.println("   | Email: " + usuario.getEmail());
+            System.out.println("   | Senha: *****" );
+            System.out.println("   =====================================");
         };
     }
 
